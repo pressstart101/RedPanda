a056ee1a844822d26f86db904a27277b15a6949e
jmkiley <jordankiley@gmail.com>
2016-07-14 10:27:11 -0400

    Added some locations and a camera

---
+++ TakeAPicture.swift
@@ -0,0 +1,11 @@
+//
+//  TakeAPictureWithARedPanda.swift
+//  MapProj
+//
+//  Created by Jordan Kiley on 7/13/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
